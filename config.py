d573daf08891b73ecebf444d14ea7fbff2cbf554
Anton Bochin <antonbocin342@gmail.com>
2022-09-19 00:44:24 +0300

    added prev and next page

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-gi
+
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you_will_never_guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POST_PER_PAGE = 3
+    POST_PER_PAGE = 10
 
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = int(os.environ.get("MAIL_PORT") or 25)
