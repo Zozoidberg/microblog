f3a4288e93ffb49038fdba1d33e409817e008f74
Anton Bochin <antonbocin342@gmail.com>
2022-09-22 00:26:02 +0300

    added .env

--- config.py
+++ config.py
@@ -1,20 +1,21 @@
+from dotenv import load_dotenv
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
+load_dotenv()
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you_will_never_guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 10
 
-    #прописать вручную#
-    MAIL_SERVER = "smtp.gmail.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = "programmingzozoidberg@gmail.com"
-    MAIL_PASSWORD = "mebatsckwvpaxpqi"
-    ADMINS = ['antonbocin@yandex.ru']
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = os.environ.get("MAIL_PORT")
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    ADMINS = os.environ.get("ADMINS")
 
-    LANGUAGES = ['en', 'ru']
+    LANGUAGES = os.environ.get("LANGUAGES")
+    print(MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD, MAIL_USE_TLS, sep='\n')
 
