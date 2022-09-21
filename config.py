6e9a23d26c3a288bb8b63dca0ec66da32a009a78
Anton Bochin <antonbocin342@gmail.com>
2022-09-22 00:12:28 +0300

    translations

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you_will_never_guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POST_PER_PAGE = 10
+    POSTS_PER_PAGE = 10
 
     #прописать вручную#
     MAIL_SERVER = "smtp.gmail.com"
@@ -15,4 +15,6 @@ class Config(object):
     MAIL_USERNAME = "programmingzozoidberg@gmail.com"
     MAIL_PASSWORD = "mebatsckwvpaxpqi"
     ADMINS = ['antonbocin@yandex.ru']
+
+    LANGUAGES = ['en', 'ru']
 
